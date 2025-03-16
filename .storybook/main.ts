299910b4f0fd8ff67ae4a47c0d94c1e282a6567c
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2025-03-16 15:54:07 +0000

    feat(graph): added storybook docs add-on and configured ts docgen

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,8 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
   stories: [
-    '../src/**/*.mdx',
-    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'
+    '../src/**/*.stories.@(tsx)'
   ],
   core: {
     disableWhatsNewNotifications: true
@@ -11,8 +10,18 @@ const config: StorybookConfig = {
   addons: [
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
-    '@storybook/experimental-addon-test'
+    '@storybook/experimental-addon-test',
+    '@storybook/addon-docs'
   ],
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => {
+        return prop.parent ? !/node_modules/.test(prop.parent.fileName) : true
+      },
+    }
+  },
   framework: {
     name: '@storybook/react-vite',
     options: {}

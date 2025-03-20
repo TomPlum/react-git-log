86d126a96566bed73cf49a67c2af59da0e160c73
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2025-03-20 14:42:56 +0000

    fix(demo): fixed potentially bad css import in storybook

--- packages/demo/.storybook/preview.ts
+++ packages/demo/.storybook/preview.ts
@@ -1,6 +1,6 @@
 import type { Preview } from '@storybook/react'
 import './preview.scss'
-import '@theme-toggles/react/css/within.css'
+import '@theme-toggles/react/css/Within.css'
 
 const preview: Preview = {
   parameters: {

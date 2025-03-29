6d0bf66ca00fb326295c088e954e26e7bac2a45a
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2025-03-29 20:34:56 +0000

    feat(demo): fixed pagination logic in demo

--- packages/demo/src/components/Pagination/Pagination.tsx
+++ packages/demo/src/components/Pagination/Pagination.tsx
@@ -1,13 +1,43 @@
 import { PaginationProps } from './types'
 import styles from './Pagination.module.scss'
 import classNames from 'classnames'
+import { useMemo } from 'react'
 
 export const Pagination = ({ theme, pageSize, currentPage, total, onChangePage }: PaginationProps) => {
   const totalPages = Math.ceil(total / pageSize)
   const pages = Array.from({ length: totalPages }, (_, i) => i + 1)
+  
+  const visiblePageButtons = useMemo(() => {
+    const pagesToShow = 11
+    const half = Math.floor(pagesToShow / 2)
+
+    let start = Math.max(1, currentPage - half)
+    let end = start + pagesToShow - 1
+
+    if (end > totalPages) {
+      end = totalPages
+      start = Math.max(1, end - pagesToShow + 1)
+    }
+
+    return pages.slice(start - 1, end)
+  }, [currentPage, pages, totalPages])
 
   return (
     <div className={styles.pagination}>
+      <button
+        disabled={currentPage === 1}
+        className={classNames(
+          styles.chevronButton,
+          { [styles.disabled]: currentPage === 1 }
+        )}
+        onClick={() => onChangePage(1)}
+        style={{
+          color: theme === 'dark' ? 'white': 'black'
+        }}
+      >
+        {'<<'}
+      </button>
+
       <button
         disabled={currentPage === 1}
         className={classNames(
@@ -23,7 +53,7 @@ export const Pagination = ({ theme, pageSize, currentPage, total, onChangePage }
       </button>
 
       <div className={styles.pages}>
-        {pages.map(page => (
+        {visiblePageButtons.map(page => (
           <button
             key={page}
             onClick={() => onChangePage(page)}
@@ -53,6 +83,20 @@ export const Pagination = ({ theme, pageSize, currentPage, total, onChangePage }
         }}
       >
         {'>'}
+      </button>
+
+      <button
+        className={classNames(
+          styles.chevronButton,
+          { [styles.disabled]: currentPage === totalPages }
+        )}
+        disabled={currentPage === totalPages}
+        onClick={() => onChangePage(totalPages)}
+        style={{
+          color: theme === 'dark' ? 'white': 'black'
+        }}
+      >
+        {'>>'}
       </button>
     </div>
   )

af66f2f8f0fa76a62e41dde51f714e01130e1dff
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2025-06-23 21:20:05 +0100

    test(graph): Added tests for custom row and node props

--- packages/library/src/modules/Table/Table.spec.tsx
+++ packages/library/src/modules/Table/Table.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { gitLog } from 'test/elements/GitLog'
 import { table } from 'test/elements/Table'
 import { Table } from 'modules/Table/Table'
@@ -80,5 +80,78 @@ describe('Table', () => {
     expect(table.timestampData({ row: 0 })).toHaveTextContent('2025 03')
 
     vi.useRealTimers()
+  })
+
+  it('should render a custom table row if one is passed', () => {
+    const selectedCommit = commit({
+      hash: '1'
+    })
+
+    const previewedCommit = commit({
+      hash: '2',
+      parents: ['1']
+    })
+
+    vi.spyOn(gitContext, 'useGitContext').mockReturnValue(gitContextBag({
+      isIndexVisible: false,
+      paging: {
+        startIndex: 0,
+        endIndex: 2,
+      },
+      selectedCommit,
+      previewedCommit,
+      graphData: graphData({
+        positions: new Map([
+          ['1', [0, 1]],
+          ['2', [1, 1]],
+        ]),
+        commits: [
+          selectedCommit,
+          previewedCommit
+        ]
+      })
+    }))
+
+    const { asFragment } = render(
+      <Table
+        row={({ commit, selected, backgroundColour, previewed }) => {
+          return (
+            <div data-testid={`custom-table-row-${commit.hash}`}>
+              <p data-testid={`custom-message-${commit.hash}`}>
+                {commit.message}
+              </p>
+
+              <p data-testid={`custom-selected-${commit.hash}`}>
+                {selected ? 'yes' : 'no'}
+              </p>
+
+              <p data-testid={`custom-previewed-${commit.hash}`}>
+                {previewed ? 'yes' : 'no'}
+              </p>
+
+              <p data-testid={`custom-bg-colour-${commit.hash}`}>
+                {backgroundColour}
+              </p>
+            </div>
+          )
+        }}
+      />
+    )
+
+    // Hash 1 is the selected commit
+    expect(screen.getByTestId('custom-table-row-1')).toBeInTheDocument()
+    expect(screen.getByTestId('custom-message-1')).toHaveTextContent(selectedCommit.message)
+    expect(screen.getByTestId('custom-selected-1')).toHaveTextContent('yes')
+    expect(screen.getByTestId('custom-previewed-1')).toHaveTextContent('no')
+    expect(screen.getByTestId('custom-bg-colour-1')).toHaveTextContent('rgba(41, 121, 255, 0.15)')
+
+    // Hash 2 is the previewed commit
+    expect(screen.getByTestId('custom-table-row-2')).toBeInTheDocument()
+    expect(screen.getByTestId('custom-message-2')).toHaveTextContent(previewedCommit.message)
+    expect(screen.getByTestId('custom-selected-2')).toHaveTextContent('no')
+    expect(screen.getByTestId('custom-previewed-2')).toHaveTextContent('yes')
+    expect(screen.getByTestId('custom-bg-colour-2')).toHaveTextContent('rgba(231, 231, 231, 0.5)')
+
+    expect(asFragment()).toMatchSnapshot()
   })
 })

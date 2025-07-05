3823ad13169884dd55d37f45fac4b11de4f20198
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2025-07-05 20:44:20 +0100

    chore(sonar): reduced cognitive complexity of VirtualEdgeRenderer below the threshold

--- packages/library/src/modules/Graph/strategies/Grid/GraphMatrixBuilder/VirtualEdgeRenderer.ts
+++ packages/library/src/modules/Graph/strategies/Grid/GraphMatrixBuilder/VirtualEdgeRenderer.ts
@@ -2,6 +2,7 @@ import { isColumnEmpty } from 'modules/Graph/strategies/Grid/utility/isColumnEmp
 import { Commit } from 'types/Commit'
 import { CommitNodeLocation } from 'data'
 import { GraphMatrix } from 'modules/Graph/strategies/Grid/GraphMatrixBuilder/GraphMatrix'
+import { GraphMatrixColumns } from 'modules/Graph/strategies/Grid/GraphMatrixBuilder/GraphMatrixColumns'
 
 export interface VirtualEdgeRendererProps {
   matrix: GraphMatrix
@@ -52,79 +53,19 @@ export class VirtualEdgeRenderer {
     // Merge commits may have lines coming out horizontally and then down to the bottom.
     // Or we may find they can draw straight down if there is free space below to the bottom.
     this._commitsWithUntrackedParents
-      .filter(commit => commit.parents.length > 1)
+      .filter(({ parents }) => parents.length > 1)
       .sort((a, b) => this._positions.get(a.hash)![0] < this._positions.get(b.hash)![0] ? -1 : 1)
-      .forEach(orphan => {
-        const [rowIndex, columnIndex] = this._positions.get(orphan.hash)!
-        const columnStates = this._matrix.getColumns(rowIndex)
-
-        // Can we just draw straight down in the current column?
-        let columnsBelowContainNode = false
-        let targetRowIndex = rowIndex + 1
-        while(targetRowIndex <= this._visibleCommits) {
-          if (this._matrix.getColumns(targetRowIndex).columns[columnIndex].isNode) {
-            columnsBelowContainNode = true
-          }
-          targetRowIndex++
-        }
-
-        if (!columnsBelowContainNode && rowIndex != this._visibleCommits) {
-          this.drawVerticalLineToBottom(orphan.hash)
-        } else {
-          // If not, we'll have to find a column to the side
-          let targetColumnIndex = columnIndex
-
-          // Find the nearest column to the right that is empty
-          while(!isColumnEmpty(columnStates.columns[targetColumnIndex])) {
-            targetColumnIndex++
-          }
-
-          // For all columns in this row up until the target, draw a horizontal line
-          for (let colIndex = columnIndex; colIndex < targetColumnIndex; colIndex++) {
-            columnStates.update(colIndex, {
-              isHorizontalLine: true,
-              mergeSourceColumns: [targetColumnIndex]
-            })
-          }
-
-          // Add the curve at the target index
-          columnStates.update(targetColumnIndex, {
-            isLeftDownCurve: true,
-            mergeSourceColumns: [targetColumnIndex]
-          })
-
-          // Finally, add vertical lines from below the curve to the bottom of the graph
-          if (rowIndex < this._visibleCommits) {
-            for (let targetRowIndex = rowIndex + 1; targetRowIndex <= this._visibleCommits; targetRowIndex++) {
-              const targetRowColumnStates = this._matrix.getColumns(targetRowIndex)
-
-              targetRowColumnStates.update(targetColumnIndex, {
-                isVerticalLine: true,
-                mergeSourceColumns: [targetColumnIndex]
-              })
-
-              this._matrix.setColumns(targetRowIndex, targetRowColumnStates)
-            }
-          }
-
-          // If we've had to draw outside the graph, then add enough virtual
-          // columns to support the new horizontal -> curve -> vertical merge lines.
-          const maxColumnIndex = this._graphWidth - 1
-          if (targetColumnIndex > maxColumnIndex) {
-            // Add a virtual column for each horizontal line drawn,
-            // plus the column with the curve and vertical lines
-            this._virtualColumns = targetColumnIndex - maxColumnIndex
-          }
-        }
-
-        this._matrix.setColumns(rowIndex, columnStates)
+      .forEach(({ hash }) => {
+        this.drawDownwardEdgesFromOrphanedMergeCommit(hash)
       })
 
     // Any commits who have child hashes that are not present in the graph and
     // are not the HEAD commit, must have vertical lines drawn from them up to
     // the top row to indicate that the child commit node is before the rows currently shown.
     this._commits.filter(commit => {
-      return commit.children.length === 0 && commit.hash !== this._headCommitHash
+      const hasNoChildren = commit.children.length === 0
+      const isNotHeadCommit = commit.hash !== this._headCommitHash
+      return hasNoChildren && isNotHeadCommit
     }).forEach(commitWithNoChildren => {
       const [rowIndex, columnIndex] = this._positions.get(commitWithNoChildren.hash)!
       for (let targetRowIndex = rowIndex; targetRowIndex >= 1; targetRowIndex--) {
@@ -144,6 +85,67 @@ export class VirtualEdgeRenderer {
     return this._virtualColumns
   }
 
+  private drawDownwardEdgesFromOrphanedMergeCommit(orphanCommitHash: string) {
+    const [rowIndex, columnIndex] = this._positions.get(orphanCommitHash)!
+    const columns = this._matrix.getColumns(rowIndex)
+
+    // Can we just draw straight down in the current column?
+    let columnsBelowContainNode = false
+    let targetRowIndex = rowIndex + 1
+    while(targetRowIndex <= this._visibleCommits) {
+      if (this._matrix.getColumns(targetRowIndex).columns[columnIndex].isNode) {
+        columnsBelowContainNode = true
+      }
+      targetRowIndex++
+    }
+
+    if (!columnsBelowContainNode && rowIndex !== this._visibleCommits) {
+      this.drawVerticalLineToBottom(orphanCommitHash)
+    } else {
+      // If not, we'll have to find a column to the side
+      let targetColumnIndex = columnIndex
+
+      // Find the nearest column to the right that is empty
+      while(!isColumnEmpty(columns.columns[targetColumnIndex])) {
+        targetColumnIndex++
+      }
+
+      // For all columns in this row up until the target, draw a horizontal line
+      this.drawHorizontalLine(columns, columnIndex, targetColumnIndex)
+
+      // Add the curve at the target index
+      columns.update(targetColumnIndex, {
+        isLeftDownCurve: true,
+        mergeSourceColumns: [targetColumnIndex]
+      })
+
+      // Finally, add vertical lines from below the curve to the bottom of the graph
+      if (rowIndex < this._visibleCommits) {
+        for (let targetRowIndex = rowIndex + 1; targetRowIndex <= this._visibleCommits; targetRowIndex++) {
+          const targetRowColumnStates = this._matrix.getColumns(targetRowIndex)
+
+          targetRowColumnStates.update(targetColumnIndex, {
+            isVerticalLine: true,
+            mergeSourceColumns: [targetColumnIndex]
+          })
+
+          this._matrix.setColumns(targetRowIndex, targetRowColumnStates)
+        }
+      }
+
+      // If we've had to draw outside the graph, then add enough virtual
+      // columns to support the new horizontal -> curve -> vertical merge lines.
+      const maxColumnIndex = this._graphWidth - 1
+      if (targetColumnIndex > maxColumnIndex) {
+        // Add a virtual column for each horizontal line drawn,
+        // plus the column with the curve and vertical lines
+        this._virtualColumns = targetColumnIndex - maxColumnIndex
+      }
+    }
+
+    this._matrix.setColumns(rowIndex, columns)
+  }
+
   private drawVerticalLineToBottom(fromCommitHash: string) {
     const [rowIndex, columnIndex] = this._positions.get(fromCommitHash)!
     for (let targetRowIndex = rowIndex; targetRowIndex <= this._visibleCommits; targetRowIndex++) {
@@ -155,6 +157,15 @@ export class VirtualEdgeRenderer {
       })
 
       this._matrix.setColumns(targetRowIndex, columnState)
+    }
+  }
+
+  private drawHorizontalLine(columns: GraphMatrixColumns, sourceColumn: number, targetColumn: number) {
+    for (let colIndex = sourceColumn; colIndex < targetColumn; colIndex++) {
+      columns.update(colIndex, {
+        isHorizontalLine: true,
+        mergeSourceColumns: [targetColumn]
+      })
     }
   }
 }

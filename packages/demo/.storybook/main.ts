e23f88f3a19ec5b179ebe24ae1e5c59acbe6cfda
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2026-01-15 20:39:25 +0000

    chore(demo): ran storybook auto-migration scripts for v10

--- packages/demo/.storybook/main.ts
+++ packages/demo/.storybook/main.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath } from 'node:url'
+import { dirname } from 'node:path'
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
@@ -11,8 +13,8 @@ const config: StorybookConfig = {
     disableWhatsNewNotifications: true
   },
   addons: [
-    '@chromatic-com/storybook',
-    '@storybook/addon-docs'
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-docs')
   ],
   typescript: {
     reactDocgen: 'react-docgen-typescript',
@@ -24,8 +26,12 @@ const config: StorybookConfig = {
     }
   },
   framework: {
-    name: '@storybook/react-vite',
+    name: getAbsolutePath('@storybook/react-vite'),
     options: {}
   }
 }
 export default config
+
+function getAbsolutePath(value: string): any {
+  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)))
+}
--- packages/demo/.storybook/preview.tsx
+++ packages/demo/.storybook/preview.tsx
@@ -1,4 +1,4 @@
-import type { Preview } from '@storybook/react'
+import type { Preview } from '@storybook/react-vite'
 import './preview.scss'
 import '@theme-toggles/react/css/Within.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'

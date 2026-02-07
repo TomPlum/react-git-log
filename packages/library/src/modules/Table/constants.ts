58102b806f65c18cfbade4112e4173a9d2975115
letnull19A <alexvolkovdd@googlemail.com>
2026-02-07 15:01:32 +0400

    fix: fixed margin of table

--- packages/library/src/modules/Table/constants.ts
+++ packages/library/src/modules/Table/constants.ts
@@ -4,12 +4,4 @@
  * data row lines up with the first row
  * on the graph. Assuming 0 row spacing.
  */
-export const HEADER_ROW_HEIGHT = 47
-
-/**
- * The number of pixels of margin at the
- * top of the table element that offsets
- * it enough to line-up correctly with
- * the graph
- */
-export const TABLE_MARGIN_TOP = 12
+export const HEADER_ROW_HEIGHT = 47;
